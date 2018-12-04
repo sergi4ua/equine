03efb315a4ccaf03a5b7320a11ac778ba946f954
sergi4ua <sergiy4ua@gmail.com>
2018-12-05 00:27:11 +0200

    0.6 - game downgrader, force update

--- EQUINE/frmForceUpdate.Designer.cs
+++ EQUINE/frmForceUpdate.Designer.cs
@@ -1,4 +1,19 @@
-﻿namespace EQUINE
+﻿/*Copyright(C) 2018 Sergi4UA
+
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see<https://www.gnu.org/licenses/>.*/
+
+namespace EQUINE
 {
     partial class frmForceUpdate
     {
@@ -72,6 +87,7 @@
             this.Name = "frmForceUpdate";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Force update";
+            this.Load += new System.EventHandler(this.frmForceUpdate_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
