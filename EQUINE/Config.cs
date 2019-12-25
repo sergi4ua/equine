c0f2fbf9e1e4054fedb1b5e5f92fdb4e613250ed
sergi4ua <39382954+sergi4ua@users.noreply.github.com>
2019-12-26 00:21:18 +0200

    Add license information

--- EQUINE/Config.cs
+++ EQUINE/Config.cs
@@ -11,7 +11,8 @@ This program is free software: you can redistribute it and/or modify
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.If not, see<https://www.gnu.org/licenses/>.*/
+    along with this program.If not, see<https://www.gnu.org/licenses/>.    
+*/
 
 namespace EQUINE
 {
--- eqmpqedit/MPQFileExtensions.cs
+++ eqmpqedit/MPQFileExtensions.cs
@@ -1,4 +1,20 @@
-﻿namespace eqmpqedit
+﻿/*Copyright(C) 2018 Sergi4UA
+
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.If not, see <https://www.gnu.org/licenses/>.
+*/
+
+namespace eqmpqedit
 {
     public class MPQFileExtensions
     {
@@ -36,6 +52,12 @@
                 return "MPEG Audio File (*.mp3)";
             if (extension == ".j")
                 return "JASS (*.j)";
+            if (extension == ".dll")
+                return "Dynamic link library (*.dll)";
+            if (extension == ".exe")
+                return "Win32 Executable (*.exe)";
+            if (extension == ".amp")
+                return "Automap file (*.amp)";
 
             return "Unknown file extension";
         }
